57c6baa097ca9266751ba1bb0129810f4f9b07df
Halvor Lyche Strandvoll <halvors@halvors.org>
2021-06-12 14:56:25 +0200

    Added libargon2 as git submodule.

--- src/libpatch/cryptoutils.cpp
+++ src/libpatch/cryptoutils.cpp
@@ -14,8 +14,8 @@ QByteArray CryptoUtils::hashPassword(const QByteArray &password)
         const uint32_t timeCost = 3;      // 3-pass computation.
         const uint32_t memoryCost = 4096; // 4 mebibytes memory usage.
         const uint32_t parallelism = 1;   // number of threads and lanes.
-        //const uint8_t salt[SALT_LENGTH] = { 0x9f, 0xfa, 0x6a, 0x64, 0x23, 0x39, 0xf7, 0x95, 0xb0, 0x87, 0x2f, 0x07, 0x0a, 0x02, 0x78, 0xe8 };
-        const char salt[SALT_LENGTH] = { 'Y', 'q', '1', 'c', 'K', 'x', 'q', 'z', 'H', 'N', '3', 's', 'k', 'C', '9', 'l' };
+        //const char salt[SALT_LENGTH] = { 'Y', 'q', '1', 'c', 'K', 'x', 'q', 'z', 'H', 'N', '3', 's', 'k', 'C', '9', 'l' };
+        const char salt[] = "Yq1cKxqzHN3skC9l";
 
         if (argon2i_hash_raw(timeCost, memoryCost, parallelism, password.constData(), password.size(), salt, SALT_LENGTH, hash.data(), hash.size()) == ARGON2_OK)
             return hash;
