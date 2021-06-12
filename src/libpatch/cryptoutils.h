ef92ccb42888ee719355f193154e6da0f367a1a5
Halvor Lyche Strandvoll <halvors@halvors.org>
2021-06-12 15:31:21 +0200

    More work.

--- src/libpatch/cryptoutils.h
+++ src/libpatch/cryptoutils.h
@@ -1,11 +1,24 @@
 #ifndef CRYPTOUTILS_H
 #define CRYPTOUTILS_H
 
+#include <cstdint>
+
+#define HASH_LENGTH 32
+
+#define TIME_COST 3         // 3-pass computation.
+#define MEMORY_COST 2 << 11 // 4 mebibytes memory usage.
+#define PARALLELISM 1       // number of threads and lanes.
+#define SALT_LENGTH 16
+
 class QByteArray;
 
-struct CryptoUtils
+class CryptoUtils
 {
+public:
     static QByteArray hashPassword(const QByteArray &password);
+
+private:
+    static const char salt[];
 };
 
 #endif // CRYPTOUTILS_H
