bc5db2e3e4d3803de8256174ee0b5a951fbfd8c6
Halvor Lyche Strandvoll <halvors@halvors.org>
2021-06-12 15:57:20 +0200

    Calculate checksum for 0.1.15 release.

--- src/common/patch_defs.h
+++ src/common/patch_defs.h
@@ -15,6 +15,9 @@ const QStringList patch_library_functions = {
     "_ZN7MPPatch18getPublicIPAddressEv@0"                         // getPublicIpAddress()
 };
 
+// Currently only applies to Steam and Uplay editions, changes game id sent to Ubisoft to that of the Retail edition.
+const QByteArray patch_game_id("2c66b725e7fb0697c0595397a14b0bc8");
+
 /**
  * Patch values patch related to community backend.
  *
@@ -38,12 +41,6 @@ const constexpr uint16_t patch_endpoint_config_port = 3074;
 const QByteArray patch_endpoint_onlineconfig = QByteArray(patch_endpoint_config_host).append(':').append(QByteArray::number(patch_endpoint_config_port)).append(3, '\0');
 const QByteArray patch_endpoint_stun_host = "stun.farcry2.online";
 
-const QByteArray patch_dev_game_id = QByteArray("88838b37e409143c9319694a6418df42");
-const QByteArray patch_dev_dedicated_server_id = QByteArray("3776f77a31dfdb6b34f6e689ee132d02");
-
-//const QByteArray patch_dev_game_id = QByteArray("2c66b725e7fb0697c0595397a14b0bc8");
-//const QByteArray patch_dev_dedicated_server_id = QByteArray("9cc10a3d6fb2cc872794b475104c204e");
-
 // Reusable assembly constants.
 const QByteArray asm_jmp("\xEB", 1);
 
@@ -63,9 +60,10 @@ const QList<FileEntry> files = {
             { // Retail (GOG is identical)
                 {
                     { "7b82f20088e5c046a99fcaed65dc8bbb8202fd622a69737be83e00686b172d53",
-                      "cd14c176f0e86cc5b362e91badf32d882e54be0ecec85057d5200206433c1cff" }
+                      "9bfb6cf3be83af54a3625df887bf6a3aeacb13dd32a683a282c4fc4f03a4d50f" }
                 },
                 {
+                    { "cd14c176f0e86cc5b362e91badf32d882e54be0ecec85057d5200206433c1cff" }, // Hash for Retail version 0.1.14.
                     { "7f0ff9ac6b07c17944df76beb563ad4d9a35cd7832f4f3f2d6aa5da1f482d9b8" }, // Hash for Retail version 0.1.13.
                     { "58b30f35da8afc9ac5de2f526d97846234d2cd332195b5a3567265702d5bb077" }, // Hash for Retail version 0.1.12.
                     { "a3ae457d942ae499d79c583f32e356cc101ffe1a221aae9220d1ac64695f3c79" }, // Hash for Retail version 0.1.11.
@@ -93,9 +91,6 @@ const QList<FileEntry> files = {
                     { 0x10e7f428, patch_endpoint_stun_host, ".rdata" },
 
                     /* Client */
-                    // Tweak: Replace game_id with dev version.
-                    { 0x10dba4c4, patch_dev_game_id, ".rdata" }, // game_id
-
                     // Fix: Hack to avoid verfiying agora certificate with public key from game files.
                     { 0x10c1354c, asm_nop(2) }, // Just importing key no matter if sig verification was success or not :-)
 
@@ -134,12 +129,14 @@ const QList<FileEntry> files = {
                 {
                     // Steam
                     { "6353936a54aa841350bb30ff005727859cdef1aa10c209209b220b399e862765",
-                      "03175df34cde45fccc02fc0c23d4aca40eef49cf228dfdc5932069a941082fa7" },
+                      "3cb9feaf36ac1c057a3fac56e57f3a60bd0f7749f26d3ad55e9366bdbdd7d696" },
                     // Uplay
                     { "b7219dcd53317b958c8a31c9241f6855cab660a122ce69a0d88cf4c356944e92",
-                      "1d8da17ef910181520a03005710fe6a2a56c10277ddecf8cab5ea4371e228c3f" }
+                      "b32d68ad40ad1e7cd7fc22836125b9b6ea25c8c2e4d17801ad8efeda58876ea3" }
                 },
                 {
+                    { "03175df34cde45fccc02fc0c23d4aca40eef49cf228dfdc5932069a941082fa7" }, // Hash for Steam version 0.1.14.
+                    { "1d8da17ef910181520a03005710fe6a2a56c10277ddecf8cab5ea4371e228c3f" }, // Hash for Uplay version 0.1.14.
                     { "98f0a42ead437a2587a2695da2a7a54aceff691c7228b8878cce54e838fc271b" }, // Hash for Steam version 0.1.13.
                     { "1f3049ee8991b639b9370b01be17c3ab9b68da6024651596520fd5e2037c11ed" }, // Hash for Uplay version 0.1.13.
                     { "d5c17109e9cc7d4f8b73804ac5a905952a858bd2ec09d359f49a46719eb30b4e" }, // Hash for Steam version 0.1.12.
@@ -174,8 +171,8 @@ const QList<FileEntry> files = {
                     { 0x10f07df8, patch_endpoint_stun_host, ".rdata" },
 
                     /* Client */
-                    // Tweak: Replace game_id with dev version.
-                    { 0x10e420c0, patch_dev_game_id, ".rdata" }, // game_id
+                    // Fix: Replace broken game id with working one.
+                    { 0x10e420c0, patch_game_id, ".rdata" }, // game_id
 
                     // Fix: Swap Ubisoft endpoints with our own.
                     { 0x10f3fa7c, patch_endpoint_onlineconfig, ".rdata" },
@@ -222,9 +219,10 @@ const QList<FileEntry> files = {
             { // Retail (GOG is identical)
                 {
                     { "c175d2a1918d3e6d4120a2f6e6254bd04907a5ec10d3c1dfac28100d6fbf9ace",
-                      "418b8393f912bb6cdede77360914fc32979964aab1950d240f15ceb35ad80da0" }
+                      "26a9f3a1cf1880df79e395006b0fb595767c10b0265283e4e9928353d30f1e79" }
                 },
                 {
+                    { "418b8393f912bb6cdede77360914fc32979964aab1950d240f15ceb35ad80da0" }, // Hash for Retail version 0.1.14.
                     { "20a85ddbff6a6a0fc35aa3ddaff9e4d08ce660000b9568731c39b3d94ac38e8d" }, // Hash for Retail version 0.1.13.
                     { "31738feedf18d2459ce8cb62589bcdc59254be7060dbbdd0d1cbe485efbacdd2" }, // Hash for Retail version 0.1.12.
                     { "9d9bbce845d81ab01821593f45783de5aba886a4133881cad265245e14247732" }, // Hash for Retail version 0.1.11.
@@ -252,9 +250,6 @@ const QList<FileEntry> files = {
                     { 0x0105fdf8, patch_endpoint_stun_host, ".rdata" },
 
                     /* Server */
-                    // Tweak: Replace game_id with dev version.
-                    { 0x01042f50, patch_dev_dedicated_server_id, ".rdata" }, // game_id
-
                     // Fix: Custom map download
                     { 0x004ecda5, asm_jmp }, // change JZ (74) to JMP (EB)
 
@@ -287,12 +282,14 @@ const QList<FileEntry> files = {
                 {
                     // Steam (R2 is identical)
                     { "5cd5d7b6e6e0b1d25843fdee3e9a743ed10030e89ee109b121109f4a146a062e",
-                      "b3d4c57e6ad51c6e4615106329a5c33d0121c25f5c5e22da80201c2488f78c5c" },
+                      "db5cfd6e70ea8abe54c221aff028132ec581684755a687e21aad7173cd57856c" },
                     // Uplay
                     { "948a8626276a6689c0125f2355b6a820c104f20dee36977973b39964a82f2703",
-                      "86a5701effea1bc0f51dc8dfe19b007bce73998b37956b3079eb58b4652c96ff" }
+                      "258d1c9461c751f245d8b8c93894ffa9957e064b2b8a87a9dec9303c21ff595f" }
                 },
                 {
+                    { "b3d4c57e6ad51c6e4615106329a5c33d0121c25f5c5e22da80201c2488f78c5c" }, // Hash for Steam version 0.1.14.
+                    { "86a5701effea1bc0f51dc8dfe19b007bce73998b37956b3079eb58b4652c96ff" }, // Hash for Uplay version 0.1.14.
                     { "0eeba5e94d07b225eca772575283897169b893b755d481e0184c20952e515442" }, // Hash for Steam version 0.1.13.
                     { "ef30f22cff98914d408ba69a6778c00e793c0b1eae128718091df206c3c1bd85" }, // Hash for Uplay version 0.1.13.
                     { "04df9d30bce8f7e22788a2fc7c6bad6719caf0f22de42f936ed7e3ed6cc1dda6" }, // Hash for Steam version 0.1.12.
@@ -325,9 +322,6 @@ const QList<FileEntry> files = {
                     { 0x01062e48, patch_endpoint_stun_host, ".rdata" },
 
                     /* Server */
-                    // Tweak: Replace game_id with dev version.
-                    { 0x01045fb0, patch_dev_dedicated_server_id, ".rdata" }, // game_id
-
                     // Fix: Custom map download
                     { 0x004eca95, asm_jmp }, // change JZ (74) to JMP (EB)
                     { QByteArray("\xE8\x4B\xCB\x2F\xFF"         // call   fc2serverlauncher.AAEB50 ; GetNetFileServerAddress()
@@ -409,10 +403,10 @@ const QList<FileEntry> files = {
                                  "\x59"                         // pop    ecx
                                  "\x58"                         // pop    eax
                                  "\xE9\x23\x29\x55\xFF", 64) }, // nop    nop nop
-                    { 0x00d049dd, QByteArray("\xE9\x9E\xD6\xAA\x00", 5).append(asm_nop(1)) }, // change function call to instead jump to the .text_p section.
+                    { 0x00d049dd, QByteArray("\xE9\x9E\xD6\xAA\x00", 5).append(asm_nop(1)) } // change function call to instead jump to the .text_p section.
 
                     /* Experimental / WIP */
-                    { 0x0052c8d3, asm_nop(2) } // bypass min player limit enforced in ranked mode.
+                    //{ 0x0052c8d3, asm_nop(2) }, // bypass min player limit enforced in ranked mode.
 
                     //{ 0x0052c8d3, asm_nop(2) }, // MinPlayers?... 75 03 // works somehow..?
                     //{ 0x00b046ff, asm_nop(2) }, // MinPlayers?... 75 03 // takes care of persist thru refresh??
