2244469315b410b319d1e1a74fce30ee24c10250
Halvor Lyche Strandvoll <halvors@halvors.org>
2021-09-23 20:12:38 +0200

    Patch with custom community backend game ids.

--- src/common/patch_defs.h
+++ src/common/patch_defs.h
@@ -42,16 +42,17 @@ const QList<QByteArray> agoraIdList = {
     "5865cbb3ffd54d7eb2ba667044b0cab1", // Ubi (Steam)
     "9cc10a3d6fb2cc872794b475104c204e", // Ubi (Dedicated server)
 
+    "3338b93611000c12bea41bcd7e9ad8c1", // Retail
+    "75eec5a819fd971e63a55c466a36211c", // Steam
+    "d4284988e671c5febefe39a09aa893c0", // Dedicated server
+
     "9c111cf209674ba51a79523f848b17fa", // Staging (Retail)
     "6d748d974900863b0bc45fe3c9d99311", // Staging (Steam)
     "ace72bbedda97396a7807ea10e1b87cf", // Staging (Dedicated server)
 
     "787292792231d5d89f9015576217b5a4", // Dev (Retail)
     "68cf276e2f1b31dfade9df215744b6dd", // Dev (Steam)
-    "d75461366f6cabb69e833dbe61fc904a", // Dev (Dedicated server)
-
-    "88838b37e409143c9319694a6418df42", // Staging (Steam) // TODO: To be removed
-    "3776f77a31dfdb6b34f6e689ee132d02"  // Staging (Dedicated server) // TODO: To be removed
+    "d75461366f6cabb69e833dbe61fc904a"  // Dev (Dedicated server)
 };
 
 // Reusable assembly constants.
@@ -73,9 +74,10 @@ const QList<FileEntry> files = {
             { // Retail (GOG is identical)
                 {
                     { "7b82f20088e5c046a99fcaed65dc8bbb8202fd622a69737be83e00686b172d53",
-                      "9bfb6cf3be83af54a3625df887bf6a3aeacb13dd32a683a282c4fc4f03a4d50f" }
+                      "ada22369c47a00b4279a70e9bf90355877e4e500410c23ff8c1e5852e4158ff6" }
                 },
                 {
+                    { "9bfb6cf3be83af54a3625df887bf6a3aeacb13dd32a683a282c4fc4f03a4d50f" }, // Hash for Retail version 0.1.16.
                     { "cd14c176f0e86cc5b362e91badf32d882e54be0ecec85057d5200206433c1cff" }, // Hash for Retail version 0.1.14.
                     { "7f0ff9ac6b07c17944df76beb563ad4d9a35cd7832f4f3f2d6aa5da1f482d9b8" }, // Hash for Retail version 0.1.13.
                     { "58b30f35da8afc9ac5de2f526d97846234d2cd332195b5a3567265702d5bb077" }, // Hash for Retail version 0.1.12.
@@ -104,11 +106,8 @@ const QList<FileEntry> files = {
                     { 0x10e7f428, patch_endpoint_stun_host, ".rdata" },
 
                     /* Client */
-                    // Tweak: Replace game_id with staging version.
-                    //{ 0x10dba4c4, agoraIdList[3], ".rdata" }, // game_id
-
-                    // Tweak: Replace game_id with dev version.
-                    { 0x10dba4c4, agoraIdList[6], ".rdata" }, // game_id
+                    // Tweak: Replace game_id with new for community backend.
+                    { 0x10dba4c4, agoraIdList[3], ".rdata" }, // game_id
 
                     // Fix: Hack to avoid verfiying agora certificate with public key from game files.
                     { 0x10c1354c, asm_nop(2) }, // Just importing key no matter if sig verification was success or not :-)
@@ -148,12 +147,15 @@ const QList<FileEntry> files = {
                 {
                     // Steam
                     { "6353936a54aa841350bb30ff005727859cdef1aa10c209209b220b399e862765",
-                      "3cb9feaf36ac1c057a3fac56e57f3a60bd0f7749f26d3ad55e9366bdbdd7d696" },
+                      "7af71319ef055fcc2193862ea43cf63dca92a957eb1c19c3956c72faaaf94804" },
+
                     // Uplay
                     { "b7219dcd53317b958c8a31c9241f6855cab660a122ce69a0d88cf4c356944e92",
-                      "b32d68ad40ad1e7cd7fc22836125b9b6ea25c8c2e4d17801ad8efeda58876ea3" }
+                      "5391e8b949ecf8b6506b925814c1d8d77d134d83660fb593645c2ed466c02282" }
                 },
                 {
+                    { "3cb9feaf36ac1c057a3fac56e57f3a60bd0f7749f26d3ad55e9366bdbdd7d696" }, // Hash for Steam version 0.1.16.
+                    { "b32d68ad40ad1e7cd7fc22836125b9b6ea25c8c2e4d17801ad8efeda58876ea3" }, // Hash for Uplay version 0.1.16.
                     { "03175df34cde45fccc02fc0c23d4aca40eef49cf228dfdc5932069a941082fa7" }, // Hash for Steam version 0.1.14.
                     { "1d8da17ef910181520a03005710fe6a2a56c10277ddecf8cab5ea4371e228c3f" }, // Hash for Uplay version 0.1.14.
                     { "98f0a42ead437a2587a2695da2a7a54aceff691c7228b8878cce54e838fc271b" }, // Hash for Steam version 0.1.13.
@@ -190,11 +192,8 @@ const QList<FileEntry> files = {
                     { 0x10f07df8, patch_endpoint_stun_host, ".rdata" },
 
                     /* Client */
-                    // Tweak: Replace game_id with staging version.
-                    //{ 0x10e420c0, agoraIdList[4], ".rdata" }, // game_id
-
-                    // Tweak: Replace game_id with dev version.
-                    { 0x10e420c0, agoraIdList[7], ".rdata" }, // game_id
+                    // Tweak: Replace game_id with new for community backend.
+                    { 0x10e420c0, agoraIdList[4], ".rdata" }, // game_id
 
                     // Fix: Swap Ubisoft endpoints with our own.
                     { 0x10f3fa7c, patch_endpoint_onlineconfig, ".rdata" },
@@ -241,9 +240,10 @@ const QList<FileEntry> files = {
             { // Retail (GOG is identical)
                 {
                     { "c175d2a1918d3e6d4120a2f6e6254bd04907a5ec10d3c1dfac28100d6fbf9ace",
-                      "26a9f3a1cf1880df79e395006b0fb595767c10b0265283e4e9928353d30f1e79" }
+                      "ed008348873b2f4f9e25ecb144aabb243b005711d9cd59841f65874c90349325" }
                 },
                 {
+                    { "26a9f3a1cf1880df79e395006b0fb595767c10b0265283e4e9928353d30f1e79" }, // Hash for Retail version 0.1.16.
                     { "418b8393f912bb6cdede77360914fc32979964aab1950d240f15ceb35ad80da0" }, // Hash for Retail version 0.1.14.
                     { "20a85ddbff6a6a0fc35aa3ddaff9e4d08ce660000b9568731c39b3d94ac38e8d" }, // Hash for Retail version 0.1.13.
                     { "31738feedf18d2459ce8cb62589bcdc59254be7060dbbdd0d1cbe485efbacdd2" }, // Hash for Retail version 0.1.12.
@@ -272,11 +272,8 @@ const QList<FileEntry> files = {
                     { 0x0105fdf8, patch_endpoint_stun_host, ".rdata" },
 
                     /* Server */
-                    // Tweak: Replace game_id with staging version.
-                    //{ 0x01042f50, agoraIdList[5], ".rdata" }, // game_id
-
-                    // Tweak: Replace game_id with dev version.
-                    { 0x01042f50, agoraIdList[8], ".rdata" }, // game_id
+                    // Tweak: Replace game_id with new for community backend.
+                    { 0x01042f50, agoraIdList[5], ".rdata" }, // game_id
 
                     // Fix: Custom map download
                     { 0x004ecda5, asm_jmp }, // change JZ (74) to JMP (EB)
@@ -310,12 +307,15 @@ const QList<FileEntry> files = {
                 {
                     // Steam (R2 is identical)
                     { "5cd5d7b6e6e0b1d25843fdee3e9a743ed10030e89ee109b121109f4a146a062e",
-                      "db5cfd6e70ea8abe54c221aff028132ec581684755a687e21aad7173cd57856c" },
+                      "18902ffab0a8227d762d0ea0bbc0ea780e52ac81b25fd6a95f41a926c9e07929" },
+
                     // Uplay
                     { "948a8626276a6689c0125f2355b6a820c104f20dee36977973b39964a82f2703",
-                      "258d1c9461c751f245d8b8c93894ffa9957e064b2b8a87a9dec9303c21ff595f" }
+                      "97dcbe2f34e21bc68560acacd4febba956abb76d3b136fcae5fdbbac5527b183" }
                 },
                 {
+                    { "db5cfd6e70ea8abe54c221aff028132ec581684755a687e21aad7173cd57856c" }, // Hash for Steam version 0.1.16.
+                    { "258d1c9461c751f245d8b8c93894ffa9957e064b2b8a87a9dec9303c21ff595f" }, // Hash for Uplay version 0.1.16.
                     { "b3d4c57e6ad51c6e4615106329a5c33d0121c25f5c5e22da80201c2488f78c5c" }, // Hash for Steam version 0.1.14.
                     { "86a5701effea1bc0f51dc8dfe19b007bce73998b37956b3079eb58b4652c96ff" }, // Hash for Uplay version 0.1.14.
                     { "0eeba5e94d07b225eca772575283897169b893b755d481e0184c20952e515442" }, // Hash for Steam version 0.1.13.
@@ -350,11 +350,8 @@ const QList<FileEntry> files = {
                     { 0x01062e48, patch_endpoint_stun_host, ".rdata" },
 
                     /* Server */
-                    // Tweak: Replace game_id with staging version.
-                    //{ 0x01045fb0, agoraIdList[5], ".rdata" }, // game_id
-
-                    // Tweak: Replace game_id with dev version.
-                    { 0x01045fb0, agoraIdList[8], ".rdata" }, // game_id
+                    // Tweak: Replace game_id with new for community backend.
+                    { 0x01045fb0, agoraIdList[5], ".rdata" }, // game_id
 
                     // Fix: Custom map download
                     { 0x004eca95, asm_jmp }, // change JZ (74) to JMP (EB)
