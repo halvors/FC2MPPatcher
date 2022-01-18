e4046a9bf58e612203e19dcc61fab03e8fa21683
Halvor Lyche Strandvoll <halvors@halvors.org>
2022-01-18 03:56:29 +0100

    More work on ASN.1 encoding.

--- src/libpatch/helper.cpp
+++ src/libpatch/helper.cpp
@@ -2,25 +2,20 @@
 
 #include <sstream>
 
-std::vector<std::string> Helper::split(const std::string& in, const char delimiter)
-{
-    std::vector<std::string> output;
-    std::istringstream stream(in);
-    std::string str;
-
-    while (std::getline(stream, str, delimiter))
-        output.push_back(str);
-
-    return output;
-}
-
 uint32_t Helper::toInt(const std::string& version)
 {
-    std::vector<std::string> list = split(version, '.');
+    std::istringstream stream(version);
+    std::string s;
 
-    uint32_t major = (std::stoi(list[0]) << 24) & 0xff000000;
-    uint32_t minor = (std::stoi(list[1]) << 16) & 0x00ff0000;
-    uint32_t patch = (std::stoi(list[2]) << 0) & 0x0000ffff;
+    uint32_t array[3] = { 0 };
+    size_t index = 0;
+
+    while (std::getline(stream, s, '.'))
+        array[index++] = std::stoi(s);
+
+    uint32_t major = (array[0] << 24) & 0xff000000;
+    uint32_t minor = (array[1] << 16) & 0x00ff0000;
+    uint32_t patch = array[2] & 0x0000ffff;
 
     return major | minor | patch;
 }
