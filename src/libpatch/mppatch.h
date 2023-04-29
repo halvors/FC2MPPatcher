90a5200b3f3565a956b61f48d2df6159fd20205d
Halvor Lyche Strandvoll <halvors@halvors.org>
2023-04-29 04:39:14 +0200

    Code cleanup

--- src/libpatch/mppatch.h
+++ src/libpatch/mppatch.h
@@ -12,11 +12,10 @@
 void read_settings();
 
 extern "C" {
-    MPPATCHSHARED_EXPORT int WSAAPI __stdcall patch_bind(SOCKET s, const sockaddr *name, int namelen);
-    MPPATCHSHARED_EXPORT int WSAAPI __stdcall patch_sendto(SOCKET s, const char* buf, int len, int flags, const sockaddr* to, int tolen);
-    MPPATCHSHARED_EXPORT uint64_t __stdcall patch_get_adapters_info(IP_ADAPTER_INFO* adapterInfo, unsigned long* sizePointer);
+    MPPATCHSHARED_EXPORT int WSAAPI __stdcall patch_bind(SOCKET s, const sockaddr* name, int namelen);
     MPPATCHSHARED_EXPORT hostent* WSAAPI __stdcall patch_get_host_by_name(const char* name);
-
+    MPPATCHSHARED_EXPORT int WSAAPI __stdcall patch_sendto(SOCKET s, const char* buf, int len, int flags, const sockaddr* to, int tolen);
+    MPPATCHSHARED_EXPORT ULONG __stdcall patch_get_adapters_info(PIP_ADAPTER_INFO adapterInfo, PULONG sizePointer);
     MPPATCHSHARED_EXPORT int __cdecl generate_cd_key_id_hex(uint8_t* out, uint32_t* outLen, const char* serialName, const char* cdKey);
     MPPATCHSHARED_EXPORT int __cdecl generate_one_time_key(uint8_t* out, uint32_t* outLen, const char* challenge, const char* username, const char* password);
 };
